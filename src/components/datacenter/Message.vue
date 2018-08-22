38a6644071ea73d6a1937cebb8cf1371fee588a0
WolfGangOnly <352964978@qq.com>
2018-08-22 08:28:53 +0800

    报文编辑页优化

--- src/components/datacenter/Message.vue
+++ src/components/datacenter/Message.vue
@@ -4,39 +4,58 @@
       <el-col :span="24">
         <div class="grid-content bg-purple-dark">
           xxx数据的报文模板
+          <el-button style="" size="small" @click="changeTextType">boom</el-button>
         </div>
+
       </el-col>
     </el-row>
-    <div class="left" style="width: 38%;float: left;">
-      <div class="messageShow" style="margin: 10px">
-        <el-input type="textarea" :autosize="{ minRows: 17, maxRows: 17}" v-model="textarea3" resize="none"></el-input>
+    <div class="left" style="width: 628px;height: 390px;float: left;overflow:auto;">
+      <div class="messageShow" style="margin: 10px;">
+        <el-input v-if="xmlTextVisible" type="textarea" :autosize="{ minRows: 17, maxRows: 17}" v-model="textarea3" resize="none"></el-input>
+        <div v-else draggable="true" :key="tag" class="el-tag" @dragstart="drag($event)" v-for="tag in dynamicTags" :disable-transitions="false" style=" margin-left: 10px;margin-top: 10px; cursor:pointer;">
+          {{tag}}     
+        </div>
       </div>
     </div>
-    <div class="right" style="width: 60%;height: 80%; float: right; overflow:auto;">
+    <div class="right" style="width: 960px;height: 380px; float: right; overflow:auto;">
       <div class="messageShow" style="margin: 15px;">
-        <el-collapse v-model="activeNames" @change="handleChange" accordion :model="formInline">
-          <el-collapse-item v-for="(dataMsg, index) in formInline.dataMsgs" :key="dataMsg.key" :prop="'dataMsg.' + index + '.title'">
+        <el-collapse v-model="activeName" accordion :model="formInline" @change="handleChange">
+          <el-collapse-item v-for="(dataMsg, index) in formInline" :key="dataMsg.key" :prop="index" :name="index">
             <template slot="title">
-              <el-input style="width: 200px" placeholder="请输入标题" class="input-new-tag" v-if="dataMsg.inputVisible" v-model="dataMsg.title" ref="saveTagInput" @keyup.enter.native="handleInputConfirm(dataMsg)" @blur="handleInputConfirm(dataMsg)" enable>
+              <el-input size="small" style="width:120px" @blur="handleInputConfirm(dataMsg)" placeholder="请输入标题" v-if="dataMsg.inputVisible" v-model="dataMsg.title" ref="inputTitle">
               </el-input>
-              <el-button v-else class="button-new-tag" @click="showInput(dataMsg)" style="width: 200px">{{dataMsg.title}}</el-button>
-              <el-button type="danger" slot="reference" size="mini" icon="el-icon-delete" circle style="margin-left: 72%"></el-button>
+              <el-tag v-else :key="dataMsg.title" :disable-transitions="true" closable @close="handleClose(dataMsg,1)">{{dataMsg.title}}</el-tag>
             </template>
             <el-form :inline="true" class="demo-form-inline">
-              <el-form-item v-for="(domain, index) in dataMsg.domains" :label="domain.label" :key="domain.key" :prop="'domains.' + index + '.value'" style="margin-left: 20px;margin-bottom: 10px">
-                <el-input v-model="domain.desc" size="small" style="width: 120px" placeholder="备注"></el-input>
+              <div  @drop="drop($event)" @dragover="allowDrop($event)">
+                 <el-form-item v-for="(domain, index) in dataMsg.domains" :key="domain.key" :prop="'domains.' + index + '.value'" style="margin-bottom: 0px">
+                <el-tag :key="domain.label" closable :disable-transitions="true" @close="handleClose(domain,2)">
+                  {{domain.label}}
+                </el-tag>
+                <el-input v-model="domain.desc" size="small" style="width: 100px" placeholder="备注"></el-input>
                 <el-input v-model="domain.value" size="small" style="width: 120px" placeholder="参数"></el-input>
               </el-form-item>
+               <el-input class="input-new-tag" v-if="newinputVisible" v-model="newinputValue" ref="newTagInput" size="small" @blur="handleNewInputConfirm('newTagInput')" style="width:120px;margin-top: 5px;">
+              </el-input>
+              <el-button v-else style="margin-top: 5px;" class="button-new-tag" size="small" @click="showInput('newTagInput')">+ New Tag</el-button>
+              </div>
+             
+             
             </el-form>
           </el-collapse-item>
         </el-collapse>
+        <el-input class="input-new-tag" v-if="newinputDataVisible" v-model="newinputDataValue" ref="newDataInput" size="small" @blur="handleNewInputConfirm('newDataInput')" style="width:120px;margin-top:5px">
+        </el-input>
+        <el-button v-else class="button-new-tag" size="small" @click="showInput('newDataInput')" style="margin-top:5px">+ New Data</el-button>
       </div>
     </div>
   </div>
 </template>
 <script>
+  let dom = null;
 export default {
   name: 'Message',
+  ready: function() {},
   mounted: function() {
 
 
@@ -47,77 +66,159 @@ export default {
   },
   data() {
     return {
-      
+      dynamicTags: ['标签1','标签2','标签3','标签4','标签5','标签6','标签7','标签8','标签9','标签10','标签11','标签12','标签13','标签14','标签15','标签16','标签17','标签18','标签19',],
       inputValue: '',
-      textarea3: '<?xml version="1.0" encoding="ISO-8859-1"?><shiporder><orderperson>George Bush</orderperson><shipto><name>Johndams</name><address>Oxford Street</address><city>London</city><country>UK</country><name1>123456</name1><name2>12345678</name2></shipto><item><title>Empire Burlesque</title><note>Special Edition</note><quantity>1</quantity><price>10.90</price></item><item><title>Hide your heart</title><quantity>1</quantity><price>9.90</price></item></shiporder>',
-      activeNames: ['1'],
-      formInline: {
-        dataMsgs: [{
-          title: '数据1',
-          inputVisible: false,
-          domains: [{
-            label: '//node1//node2',
-            desc: '',
-            value: ''
-          }, {
-            label: '//node1//node2//',
-            desc: '',
-            value: ''
-          }, {
-            label: '//node1//node2//node3//node4//node5//node6',
-            desc: '',
-            value: ''
-          }, {
-            label: '参数4',
-            desc: '',
-            value: ''
-          }]
-        },{
-          title: '数据2',
-          inputVisible: true,
-          domains: [{
-            label: '//node1//node2',
-            desc: '金额',
-            value: ''
-          }, {
-            label: '//node1//node2//',
-            desc: '',
-            value: ''
-          }, {
-            label: '//node1//node2//node3//node4//node5//node6',
-            desc: '',
-            value: ''
-          }, {
-            label: '参数4',
-            desc: '',
-            value: ''
-          }]
+      newinputDataValue: '',
+      newinputDataVisible: false,
+      newinputValue: '',
+      newinputVisible: false,
+
+      xmlTextVisible:true,
+      textarea3: '<?xml version="1.0" encoding="UTF-8"?><shiporder><orderperson>George Bush</orderperson><shipto><name>Johndams</name><address>Oxford Street</address><city>London</city><country>UK</country><name1>123456</name1><name2>12345678</name2></shipto><item><title>Empire Burlesque</title><note>Special Edition</note><quantity>1</quantity><price>10.90</price></item><item><title>Hide your heart</title><quantity>1</quantity><price>9.90</price></item></shiporder>',
+      activeName: '1',
+
+      formInline: [{
+        title: '数据1',
+        inputVisible: false,
+        domains: [{
+          label: '//node1//node2',
+          desc: '',
+          value: ''
+        }, {
+          label: '//node1//node2//',
+          desc: '',
+          value: ''
+        }, {
+          label: '//node1//node2//node3//node4//node5//node6',
+          desc: '',
+          value: ''
+        }, {
+          label: '参数4',
+          desc: '',
+          value: ''
         }]
-
-
-      },
-
+      }, {
+        title: '数据2',
+        inputVisible: false,
+        domains: [{
+          label: '//node1//node2',
+          desc: '金额',
+          value: ''
+        }, {
+          label: '//node1//node2//',
+          desc: '',
+          value: ''
+        }, {
+          label: '//node1//node2//node3//node4//node5//node6',
+          desc: '',
+          value: ''
+        }, {
+          label: '参数4',
+          desc: '',
+          value: ''
+        }]
+      }],
+      //存放结构
+      formInlineTemp: [],
+      
     }
   },
   methods: {
+    //切换xml形态
+    changeTextType() {
+      if (this.xmlTextVisible) {
+        this.xmlTextVisible = false;
+      } else {
+        this.xmlTextVisible = true;
+      }
+    },
+    drag: function(event) {
+      this.newinputValue = event.target.innerText;
+      dom = event.currentTarget
+    },
+    drop: function(event) {
+      event.preventDefault();
+
+      //event.target.appendChild(dom);
+      this.handleNewInputConfirm("newTagInput");
+      
+    },
+    allowDrop: function(event) {
+      event.preventDefault();
+
+    },
+    handleClose(val, index) {
+      if (index === 1) {
+        this.formInline.splice(this.formInline.indexOf(val), 1);
+      } else {
+        this.formInline[this.activeName].domains.splice(this.formInline[this.activeName].domains.indexOf(val), 1);
+      }
+
+    },
     handleChange(val) {
-      console.log(val);
+      if (val !== '') {
+
+        for (var i = this.formInline.length - 1; i >= 0; i--) {
+          if (this.formInline[i].inputVisible) {
+            this.formInline[i].inputVisible = false;
+          }
+        }
+        this.formInline[val].inputVisible = true;
+        this.$nextTick(_ => {
+          this.$refs.inputTitle[0].$refs.input.focus();
+        });
+
+      }
+
+
     },
     showInput(val) {
-        val.inputVisible = true;
+      if (val === 'newTagInput') {
+        this.newinputVisible = true;
         this.$nextTick(_ => {
-          this.$refs.saveTagInput.$refs.input.focus();
+          this.$refs.newTagInput[this.activeName].$refs.input.focus();
         });
-      },
-      handleInputConfirm(val) {
-        //let inputValue1 = this.$refs.saveTagInput.value;
-        // if (inputValue) {
-        //   this.dynamicTags.push(inputValue);
-        // }
-        console.log(val);
-        val.inputVisible = false;
-        //val.title = 'dsad';
-      },
+      } else {
+        this.newinputDataVisible = true;
+        this.$nextTick(_ => {
+          this.$refs.newDataInput.$refs.input.focus();
+        });
+      }
+    },
+    handleInputConfirm(val) {
+      //let inputValue1 = this.$refs.saveTagInput.value;
+      // if (inputValue) {
+      //   this.dynamicTags.push(inputValue);
+      // }
+      console.log(val);
+      val.inputVisible = false;
+      //val.title = 'dsad';
+    },
+    handleNewInputConfirm(val) {
+      //新增一个参数
+      if (val === 'newTagInput') {
+        console.log(this.newinputValue);
+        let inputValue = this.newinputValue;
+        if (inputValue) {
+          let newData = {
+            label: inputValue,
+            desc: '',
+            value: ''
+          }
+          this.formInline[this.activeName].domains.push(newData);
+        }
+        this.newinputValue = '';
+        this.newinputVisible = false;
+      }
+      //新增一组数据 
+      else {
+        let data = this.formInline.dataMsgs[0];
+        this.formInline.dataMsgs.push(data);
+        this.newinputDataVisible = false;
+      }
+
+
+    },
   }
 
 }
--- src/components/page/Home.vue
+++ src/components/page/Home.vue
@@ -1,13 +1,111 @@
 <template>
-	<div class="home">这是首页</div>
-  
+	<div class='drag-content'>
+		<div class='project-content'>
+			<div class='select-item'  draggable="true" @dragstart="drag($event)"  v-for="tag in projectdatas">
+				 <el-tag :key="tag" :disable-transitions="false" style=" margin-left: 10px;margin-top: 10px">{{tag}}</el-tag>
+			</div>
+		</div>
+		<div class='people-content'>
+			<div class='drag-div' v-for='(ppdt,ppindex) in peopledata' @drop='drop($event)' @dragover='allowDrop($event)'>
+				<div class='select-project-item'>
+					<label class='drag-people-label'>{{ppdt.name}}:</label>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
+	let dom = null;
 export default {
 	name: 'Home',
+	methods: {
+		drag: function(event) {
+			console.log(event);
+			dom = event.currentTarget
+		},
+		drop: function(event) {
+			event.preventDefault();
+			event.target.appendChild(dom);
+		},
+		allowDrop: function(event) {
+			event.preventDefault();
+		},
+	},
+	data() {
+		return {
+			projectdatas: [{
+				id: 1,
+				name: '葡萄',
+			}, {
+				id: 2,
+				name: '芒果',
+			}, {
+				id: 3,
+				name: '木瓜',
+			}, {
+				id: 4,
+				name: '榴莲',
+			}],
+			peopledata: [{
+				id: 1,
+				name: '小颖',
+			}, {
+				id: 2,
+				name: 'hover',
+			}, {
+				id: 3,
+				name: '空巢青年三 ',
+			}, {
+				id: 3,
+				name: '一丢丢',
+			}],
+		}
+	}
 }
+
 </script>
 <style>
+.select-item {
+	background-color: #5bc0de;
+	display: inline-block;
+	text-align: center;
+	border-radius: 3px;
+	margin-right: 10px;
+	cursor: pointer;
+	padding: 6px 20px;
+	color: #fff;
+}
 
+.cursored {
+	cursor: default;
+}
+
+.project-content,
+.people-content {
+	margin: 30px 50px;
+}
+
+.people-content {
+	margin-top: 30px;
+}
+
+.drag-div {
+	border: 1px solid #5bc0de;
+	padding: 10px;
+	margin-bottom: 10px;
+	width: 800px;
+	cursor: pointer;
+}
+
+.select-project-item {
+	display: inline-block;
+	text-align: center;
+	border-radius: 3px;
+}
+
+.drag-people-label {
+	margin-bottom: 0;
+	padding-right: 10px;
+}
 
 </style>
